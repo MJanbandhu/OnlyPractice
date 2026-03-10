c00517f5943b23603112e300a24120eb92f6d056
MJanabndhu <vinodjanbandhu3@gmail.com>
2026-03-11 00:25:02 +0530

    V0.1.26

---
+++ R_Prog/Basic.R
@@ -0,0 +1,27 @@
+mo <- 5+6
+
+
+data <- c(10,20,30,40,60,50,70)
+
+mean_data <- mean(data)
+print(paste("Mean: ", mean_data))
+
+
+
+
+data <- c(10,20,30,40,60,50,70)
+mean(data)
+
+
+
+data <- c(10,20,30,40,60,50,70)
+
+mean_data <- mean(data)
+print(paste("Mean:", mean_data))
+
+
+
+
+
+
+print("Mohit Janbandhu")
